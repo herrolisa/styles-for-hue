37ff66fa35c8b72c0841e18d2baafc11527b7c38
Lisa <misslisazhou@gmail.com>
2017-03-21 20:28:20 -1000

    removed reference to former username, update with new username

--- src/containers/About.js
+++ src/containers/About.js
@@ -39,7 +39,7 @@ class About extends Component {
             <div className="flex-photos pos-2">
               <div className="profile"/>
               <h3>
-                <a className="link-to-github" href="https://github.com/herrolisa" target="_blank">
+                <a className="link-to-github" href="https://github.com/lisazhou" target="_blank">
                   Lisa Zhou
                 </a>
               </h3>
